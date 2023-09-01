1c8f4cf3d5ed165ad018e2d8157d54590f306d92
achrefabdennebi <achref.abdennebi@gmail.com>
2023-09-01 23:15:06 +0200

    feat: edit rss xml reader fn

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,9 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 
-export async function GET() {
-  return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://first-tuto-astro.netlify.app/',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
-}
+export const get = () => rss({
+  title: 'Astro Learner | Blog',
+  description: 'My journey learning Astro',
+  site: 'https://first-tuto-astro.netlify.app/',
+  items: import.meta.glob('./**/*.md'),
+  customData: `<language>en-us</language>`,
+})
